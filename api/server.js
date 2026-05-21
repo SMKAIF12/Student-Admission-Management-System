78a4c6feef095d1da47f1502161927017f9472be
kaif <103161771+SMKAIF12@users.noreply.github.com>
2026-05-21 15:57:07 +0530

    Updated files

--- api/server.js
+++ api/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./connection/connection');
-const adminRoutes = require('./routes/admin-routes');
-const authRoutes = require('./routes/auth-routes');
-const applicationRoutes = require('./routes/applicant-routes');
+// const adminRoutes = require('./routes/admin-routes');
+// const authRoutes = require('./routes/auth-routes');
+// const applicationRoutes = require('./routes/applicant-routes');
+const routes = require('./routes/routes')
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 connectDB();
-app.use('/admin',adminRoutes);
-app.use('/',authRoutes);
-app.use('/application',applicationRoutes)
+app.use('/admin',routes);
+app.use('/',routes);
+app.use('/application',routes)
 app.listen(PORT, () => {
     console.log('Server listening on:', PORT);
 })
